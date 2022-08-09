30840c1befdfd66f0d4510dea446b78be2978f71
swillrich5 <swillrich@gmail.com>
2022-08-09 09:18:10 -0500

    Navbar added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
+import Navbar from './components/Navbar';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
+
 
 function App() {
   return (
-    <div>
-        <h1>Rick & Morty in the House!!!</h1>
-    </div>
+    <Router >
+        <Navbar />
+        <div>
+            <h1>Rick & Morty in the House!!!</h1>
+        </div>
+    </Router>
+
   );
 }
 
