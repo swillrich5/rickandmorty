5f57e4d83577110619277b9c3b5886d16fb85a54
swillrich5 <swillrich@gmail.com>
2022-08-11 08:53:59 -0500

    mapping array of episodes to character page (rough)

--- src/components/Episodes.js
+++ src/components/Episodes.js
@@ -5,9 +5,28 @@ const Episodes = ({ episodes }) => {
     console.log("============> Episodes <===========");
     console.log(episodes);
     console.log("============> Episodes <===========");
+    const [episodeObject, setEpisodeObject] = useState({episodes});
+    let episodeString = '';
+    for (let i=0; i < episodes.length; i++) {
+        episodeString = episodeString + episodes[i].split("https://rickandmortyapi.com/api/episode/")[1];
+        if (i < (episodes.length - 1)) {
+            episodeString += ','
+        }
+    }
+    console.log(episodeString);
+    console.log(episodeObject);
+
 
   return (
-    <div>This is where all of the episodes will list out</div>
+    <div>
+        <div className="row justify-content-center pb-3">
+            {episodes.map(episode => 
+                <div className='col' key={episode}>
+                    <p>Episode {episode.split("https://rickandmortyapi.com/api/episode/")[1]}</p>
+                </div>
+            )};
+        </div>
+    </div>
   )
 }
 
