7bef5804b60fd416e101d1dfc546545048665674
Scott Willrich <swillrich@gmail.com>
2022-09-08 09:17:26 -0500

    code cleanup - removing unused components

--- src/pages/Character.js
+++ src/pages/Character.js
@@ -1,6 +1,5 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { useLocation } from "react-router-dom";
-import Spinner from '../components/Spinner';
 import Episodes from '../components/Episodes'
 import './Character.css';
 
@@ -8,44 +7,39 @@ const Character = () => {
 
     const location = useLocation();
 
-    const [loading, setLoading] = useState(false);
     const character = location.state.character;
-    const episodes = character.episode;
 
-  if (loading) {
-    return <Spinner />
-  } else {
-        return (
-            <div className="container space-background">
-            <div className="jumbotron">
-                <div className="row justify-content-center mt-3 pb-3">
-                    <div className="col">
-                        <img src={character.image} className="shadow-lg rounded" alt="character"/>
-                        <h1>{character.name}</h1>
-                        <div className='mx-5 px-5'>
-                            <table className='table table-borderless table-sm'>
-                                <tbody>
-                                    <tr>
-                                        <td>Origin: {character.origin.name}</td>
-                                        <td>Species: {character.species}</td>
-                                    </tr>
-                                    <tr>
-                                        <td>Gender: {character.gender}</td>
-                                        <td>Status: {character.status}</td>
-                                    </tr>  
-                                </tbody>
-                            </table>                            
-                        </div>
-                        <div>
-                            <hr />
-                            <h2>Episodes</h2>
-                            <Episodes episodes={character.episode}/>
-                        </div>
+    return (
+        <div className="container space-background">
+        <div className="jumbotron">
+            <div className="row justify-content-center mt-3 pb-3">
+                <div className="col">
+                    <img src={character.image} className="shadow-lg rounded" alt="character"/>
+                    <h1>{character.name}</h1>
+                    <div className='mx-5 px-5'>
+                        <table className='table table-borderless table-sm'>
+                            <tbody>
+                                <tr>
+                                    <td>Origin: {character.origin.name}</td>
+                                    <td>Species: {character.species}</td>
+                                </tr>
+                                <tr>
+                                    <td>Gender: {character.gender}</td>
+                                    <td>Status: {character.status}</td>
+                                </tr>  
+                            </tbody>
+                        </table>                            
+                    </div>
+                    <div>
+                        <hr />
+                        <h2>Episodes</h2>
+                        <Episodes episodes={character.episode}/>
                     </div>
                 </div>
             </div>
         </div>
-  )}
+    </div>
+  )
 }
 
 export default Character;
